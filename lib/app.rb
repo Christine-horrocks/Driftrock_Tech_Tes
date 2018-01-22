5db48fbff2e52964b583ea08c34582fce5ce8a55
Christine <splogy2001@hotmail.com>
2018-01-22 18:43:25 +0000

    command total_spend tested and added

--- lib/app.rb
+++ lib/app.rb
@@ -4,20 +4,25 @@ class DataManager
 
   def initialize
     @user_data = [
-      {
-      "id": "12DF-2324-GA2D-31RT", "first_name": "Drift", "last_name": "Rock",
-      "phone": "0-200-100-1234", "email": "drift.rock@email.com"
-      },
-      {
-      "id": "33DF-2324-GA2D-31RT", "first_name": "Glide", "last_name": "Rock",
-      "phone": "0-333-100-1234", "email": "glide.rock@email.com"
-      },
-       ]
+      { "id": "12DF-2324-GA2D-31RT", "first_name": "Drift", "last_name": "Rock",
+      "phone": "0-200-100-1234", "email": "drift.rock@email.com" },
+      { "id": "33DF-2324-GA2D-31RT", "first_name": "Glide", "last_name": "Rock",
+      "phone": "0-333-100-1234", "email": "glide.rock@email.com" },
+      ]
+
+    @purchase_data = [
+      { "user_id": "12DF-2324-GA2D-31RT", "item": "Huge pineapple pizza", "spend": "19.99"},
+      {"user_id": "12DF-2324-GA2D-31RT", "item": "A nice cup of tea", "spend": "2.99"},
+      {"user_id": "33DF-2324-GA2D-31RT", "item": "A nice cup of tea", "spend": "2.99"}
+      ]
   end
 
   def total_spend(em)
     user = @user_data.select { |user| user[:email] == em }
-    user[0].fetch(:id)
+    user_id = user[0].fetch(:id)
+    items = @purchase_data.select { |purchase| purchase[:user_id] == user_id}
+    total = items.map { |item| item.fetch(:spend).to_f }
+    total.sum.round(2)
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,6 +3,6 @@ require 'app.rb'
 describe DataManager do
 
   it 'identifies the user_id from the email' do
-    expect(subject.total_spend('drift.rock@email.com')).to eq("12DF-2324-GA2D-31RT")
+    expect(subject.total_spend('drift.rock@email.com')).to eq(22.98)
   end
 end
