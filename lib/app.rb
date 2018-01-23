4007768732e25c3aea21432a5f507c10ef9bfe16
Christine <splogy2001@hotmail.com>
2018-01-23 15:59:29 +0000

    most loyal tested and added

--- lib/app.rb
+++ lib/app.rb
@@ -17,12 +17,30 @@ class DataManager
       ]
   end
 
-  def total_spend(em)
-    user = @user_data.select { |user| user[:email] == em }
+  def total_spend(email)
+    user = @user_data.select { |user| user[:email] == email }
     user_id = user[0].fetch(:id)
     items = @purchase_data.select { |purchase| purchase[:user_id] == user_id}
-    total = items.map { |item| item.fetch(:spend).to_f }
-    total.sum.round(2)
+    spends = items.map { |item| item.fetch(:spend).to_f }
+    spends.sum.round(2)
+  end
+
+  def average_spend(email)
+    user = @user_data.select { |user| user[:email] == email }
+    user_id = user[0].fetch(:id)
+    items = @purchase_data.select { |purchase| purchase[:user_id] == user_id}
+    spends = items.map { |item| item.fetch(:spend).to_f }
+    total = spends.sum.round(2)
+    average = total / spends.count
+    average.round(2)
+  end
+
+  def most_loyal
+    user_ids = @purchase_data.map { |purchase| purchase.fetch(:user_id) }
+    freq = user_ids.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    most_loyal_id = user_ids.max_by { |v| freq[v] }
+    user = @user_data.select { |user| user[:id] == most_loyal_id }
+    user_id = user[0].fetch(:email)
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,7 +2,17 @@ require 'app.rb'
 
 describe DataManager do
 
-  it 'identifies the user_id from the email' do
+  it 'returns the email users total spend' do
     expect(subject.total_spend('drift.rock@email.com')).to eq(22.98)
   end
+
+  it 'returns the email users average spend' do
+    expect(subject.average_spend('drift.rock@email.com')).to eq(11.49)
+  end
+
+  it 'identifies the user with most purchases' do
+    expect(subject.most_loyal).to eq('drift.rock@email.com')
+  end
+
+
 end
